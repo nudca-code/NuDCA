0fa949c8b751c26284fc4054fc24407a7ed094cc
Infinity <chohonchen@163.com>
2025-07-05 15:22:40 +0800

    Update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,15 +16,59 @@ release = version
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.mathjax',
+    'sphinx_copybutton',
+    'sphinx_design',
+    'sphinx_tags',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_book_theme'
 html_static_path = ['_static']
+
+# Theme options
+html_theme_options = {
+    "repository_url": "https://github.com/infinityo0o/NuDCA",
+    "use_repository_button": True,
+    "use_issues_button": True,
+    "use_edit_page_button": True,
+    "path_to_docs": "docs/source",
+}
+
+# -- Extension configuration -------------------------------------------------
+
+# Napoleon settings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+
+# Intersphinx mapping
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'numpy': ('https://numpy.org/doc/stable/', None),
+    'scipy': ('https://docs.scipy.org/doc/scipy/', None),
+    'matplotlib': ('https://matplotlib.org/stable/', None),
+    'pandas': ('https://pandas.pydata.org/docs/', None),
+}
+
+# Autodoc settings
+autodoc_default_options = {
+    'members': True,
+    'undoc-members': True,
+    'show-inheritance': True,
+}
+
+# Copy button settings
+copybutton_prompt_text = ">>> "
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -3,15 +3,71 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-NuDCA documentation
+NuDCA Documentation
 ===================
 
-Add your content using ``reStructuredText`` syntax. See the
-`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
-documentation for details.
+Welcome to NuDCA's documentation!
 
+NuDCA is a Python package for nuclear decay chain analysis and kilonova modeling.
 
-.. toctree::
-   :maxdepth: 2
-   :caption: Contents:
+Quick Start
+-----------
+
+Installation
+~~~~~~~~~~~~
+
+You can install NuDCA from PyPI:
+
+.. code-block:: bash
+
+   pip install nudca
+
+Or install from source:
+
+.. code-block:: bash
+
+   git clone https://github.com/yourusername/NuDCA.git
+   cd NuDCA
+   pip install -e .
+
+Basic Usage
+~~~~~~~~~~~
+
+Here's a simple example to get you started:
+
+.. code-block:: python
+
+   import nudca
+   
+   # Your code here
+   pass
+
+Features
+--------
+
+- 🔬 Nuclear decay chain analysis
+- 💫 Kilonova modeling
+- 📊 Data visualization
+- 🧮 Scientific computing tools
+
+Project Structure
+-----------------
+
+- ``nudca/`` - Main package directory
+- ``examples/`` - Example notebooks and scripts
+- ``data/`` - Data files and datasets
+- ``tests/`` - Unit tests
+- ``docs/`` - Documentation source files
+
+Development
+-----------
+
+To contribute to NuDCA, please see our GitHub repository.
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
 
